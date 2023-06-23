51f2d596f0fcdc141852ea14af011f223c73fc33
nadiapputrii <nadiapputriirah@gmail.com>
2023-06-23 21:13:34 +0700

    Online store website

---
+++ resources/views/includes/navbar-authenticated.blade.php
@@ -0,0 +1,78 @@
+<!-- Navigation -->
+    <nav
+      class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top"
+      data-aos="fade-down"
+      aria-label="Navbar"
+    >
+      <div class="container">
+        <a class="navbar-brand" href="/">
+          <img src="/images/logo.svg" alt="" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/index.html">Home </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/categories.html">Categories</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Rewards</a>
+            </li>
+
+            <!-- Dekstop Menu -->
+            <ul class="navbar-nav d-none d-lg-flex">
+              <li class="nav-item dropdown">
+                <a
+                  href="#"
+                  class="nav-link"
+                  id="navbarDropdown"
+                  role="button"
+                  data-toggle="dropdown"
+                >
+                  <img
+                    src="/images/profile.jpg"
+                    alt=""
+                    class="rounded-circle mr-2 profile-picture"
+                  />
+                  Hi, Angga
+                </a>
+                <div class="dropdown-menu">
+                  <a href="/dashboard.html" class="dropdown-item">Dashboard</a>
+                  <a href="/dashboard-account.html" class="dropdown-item"
+                    >Settings</a
+                  >
+                  <div class="dropdownd-driver"></div>
+                  <a href="/" class="dropdown-item">Logout</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link d-inline-block mt-2">
+                  <img src="/images/icon-cart-empty.svg" alt="" />
+                </a>
+              </li>
+            </ul>
+          </ul>
+
+          <ul class="navbar-nav d-block d-lg-none">
+            <li class="nav-item">
+              <a href="#" class="nav-link"> Hi, Angga </a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link d-inline-block"> Cart </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
